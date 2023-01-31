e26853abbedc0407f3a43a88c3298412d1509d92
amlaketadesse <amlaketadesse@gmail.com>
2023-01-30 21:23:31 -0800

    lol

---
+++ app.js
@@ -0,0 +1,31 @@
+const searchBtn = document.getElementById('search__btn');
+const movieList = document.getElementById('movies');
+
+
+searchBtn.addEventListener('click', getMovieList);
+
+async function getMovieList(){
+    let searchInputTxt = document.getElementById ('search__input').value.trim();
+    const movies = await fetch(`https://www.omdbapi.com/?apikey=4d79dfe6&s=${searchInputTxt}`);
+    const moviesData = await movies.json();
+    console.log(moviesData)
+    movieList.innerHTML = moviesData.results?.map(movie => getMovieList(movie)).join('')
+
+
+        function getMovieList(movie) {
+                 `<div class="movie" data-id = ${movie.imdbID}>
+                        <figure class="movie__img--wrapper">
+                            <img src="${movie.Poster}" class="movie__img">
+                        </figure>
+                        <div class="movie__title">
+                            ${movie.Title}
+                        </div>
+                        <div class="movie__year">
+                            <p>${movie.Year}</p>
+                        </div>
+                    </div>`.join('');
+        
+        }
+}
+
+getMovieList();
